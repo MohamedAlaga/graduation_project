bed51e3f599628f33b67b81d5a857512fda6f869
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2024-02-02 03:48:58 +0200

    Front-end app set up done!

---
+++ AbkarFE/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Login - Signup flow */}
+
+          {/* Home flow */}
+
+          {/* <Route path="/" element={} /> */}
+
+          {/* Quiz flow */}
+
+          {/* Settings flow */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
